8b24551adf6f19e8cec24d9851836e30f709f689
William Moon <william-moon@lambdastudents.com>
2021-03-25 13:07:55 -0700

    finished task 4

--- index.js
+++ index.js
@@ -120,6 +120,7 @@ class Airplane {
       return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
   }
+
   
   /*
     TASK 4
@@ -135,8 +136,19 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
-
+ class Instructor extends Lambdasian {
+   constructor(attribute){
+     super(attribute);
+     this.specialty = attribute.specialty;
+     this.favLanguage = attribute.favLanguage;
+     this.catchPhrase = attribute.catchPhrase;
+   }
+   demo(subject){
+     return `Today we are learning about ${subject}`;
+   }
+   grade(student, subject){
+     return `${student.name} receives a perfect score on ${subject}`;
+   }
  }
   /*
     TASK 5
